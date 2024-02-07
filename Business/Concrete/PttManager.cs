3b5343b3c61659b765cc9a307bef6db284317147
oguzhandilek <oguzhandilek25@gmail.com>
2024-02-08 00:13:53 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+
+public class PttManager:ISuppllierService
+{
+    private readonly IApplicantService _applicantService;
+
+    public PttManager(IApplicantService applicantService)
+    {
+        _applicantService = applicantService;
+    }
+
+    public void GiveMask(Person person)
+    {
+        
+       
+        if (_applicantService.CheckPerson(person))
+        {
+            Console.WriteLine(person.FirstName+" için Maske verildi");
+        }
+        else
+        {
+            Console.WriteLine(person.FirstName + " için Maske verilemedi");
+        }
+
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using Entities.Concrete;
+
+Console.WriteLine("Hello, World!");
+Topla(1, 3);
+static int Topla(int sayi1, int sayi2)
+{
+    int result = sayi1 + sayi2;
+    Console.WriteLine("Toplam: " + result);
+    return result;
+    
+}
+Person person = new Person();
+person.NationalIdentity = 123;
+person.FirstName = "Oğuzhan";
+person.LastName = "Dilek";
+person.DateOfBirthYear = 1992;
+
+
+PttManager pttManager=new PttManager(new PersonManager());
+pttManager.GiveMask(person);
+Console.ReadLine();
